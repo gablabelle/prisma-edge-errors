c94c0ba9301d9712b8d1498c053fccf7fb40d318
Gab Labelle <gab@visualise.ca>
2024-03-18 19:25:51 -0400

    added missing cookie

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -37,6 +37,44 @@ export const authOptions: NextAuthConfig = {
       return !!auth?.user; // this ensures there is a logged in user for -every- request
     },
   },
+  cookies: {
+    sessionToken: {
+      name: `__Secure-next-auth.session-token`,
+      options: {
+        secure: true,
+        httpOnly: true,
+        path: "/",
+        partitioned: true,
+      },
+    },
+    callbackUrl: {
+      name: `__Secure-next-auth.callback-url`,
+      options: {
+        secure: true,
+        httpOnly: true,
+        path: "/",
+        partitioned: true,
+      },
+    },
+    csrfToken: {
+      name: `next-auth.csrf-token`,
+      options: {
+        secure: true,
+        httpOnly: true,
+        path: "/",
+        partitioned: true,
+      },
+    },
+    pkceCodeVerifier: {
+      name: "next-auth.pkce.code_verifier",
+      options: {
+        secure: true,
+        httpOnly: true,
+        path: "/",
+        partitioned: true,
+      },
+    },
+  },
 };
 
 export const {
