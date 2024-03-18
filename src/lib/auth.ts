9aba741e4a03c0c546cabbb6113e7fd36e8c3241
Gab Labelle <gab@visualise.ca>
2024-03-18 18:58:59 -0400

    Updated libs

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,4 +1,4 @@
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import NextAuth, { type NextAuthConfig } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { db } from "@/lib/db";
@@ -7,9 +7,6 @@ export const authOptions: NextAuthConfig = {
   adapter: PrismaAdapter(db),
   session: { strategy: "database" }, // switch the strategy to "jwt" to avoid RSC throwing 500s
   secret: process.env.AUTH_SECRET,
-  jwt: {
-    secret: process.env.AUTH_SECRET,
-  },
   providers: [GithubProvider],
   callbacks: {
     async jwt({ token, user }) {
